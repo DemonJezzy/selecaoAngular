2bf2b8f053cb9b44a51c9dd50871da3e4fd552b6
DemonJezzy <nomanhafeez667@gmail.com>
2021-11-18 10:52:44 +0500

    first Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarasoulComponent } from './carasoul/carasoul.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { FaqComponent } from './faq/faq.component';
+import { FooterComponent } from './footer/footer.component';
+import { TeamComponent } from './team/team.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarasoulComponent,
+    AboutComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    PricingComponent,
+    FaqComponent,
+    FooterComponent,
+    TeamComponent,
+    ContactusComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
